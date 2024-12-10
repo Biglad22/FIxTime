c48f6450b4d79dad0320b4fa4460e9d3743a831b
Biglad22 <emmanuelaberuagba22@gmail.com>
2024-12-10 23:55:00 +0100

    second commit

--- app.jsx
+++ app.jsx
@@ -13,7 +13,7 @@ const nav_links_data = [
         link: 'https://x.com'
     },
     {   
-        title : 'trade $SAMPLE',
+        title : 'trade $FLXT',
         link: 'https://x.com'
     }, 
 ]
@@ -59,6 +59,25 @@ const flex_cards = [
       icon: "./assets/icon/cards.svg", // Path to the image
     },
 ];
+
+const token_map = [
+    {
+        title : '0% to allocation',
+        icon : './assets/img/community-img.png'
+    },
+    {
+        title : '0% to allocation',
+        icon : './assets/img/reward-img.png'
+    },
+    {
+        title : '0% to allocation',
+        icon : './assets/img/reserve-img.png'
+    },
+    {
+        title : 'X total supply',
+        icon : './assets/img/timeframe-img.png'
+    },
+]
   
 
 
@@ -67,19 +86,21 @@ const flex_cards = [
 function App (){
     return(
         <div className="bg-[#003338] ">
-            <div className="flex flex-col relative gap-6 mx-0 w-full px-[8vw] min-[1500px]:px-0 min-[1500px]:w-[1300px] min-[1500px]:mx-auto">
+            <div className="flex flex-col relative gap-0 mx-0 w-full px-[1rem] sm:px-[2rem] min-[1500px]:px-0 min-[1500px]:w-[1300px] min-[1500px]:mx-auto">
                 
-                <div className="h-[100vh] flex flex-col items-center py-4  gap-6 w-full">
-                    <NavBar />
-                    <HeroSection className='flex-1' />
-                </div>
-                <ProjectDesc />
-                <Separator />
-                <Flexer />
-                <TokenomicsSection />
-                <StakeSection />
-                <Separator />
-                <Footer />
+                <NavBar />
+                <HeroSection className='flex-1 py-4 w-full min-h-[82vh] my-4 sm:my-8 mt-2' />
+                {/* <div className="h-[100vh] flex flex-col items-center py-4  gap-6 w-full">
+                    
+                    
+                </div> */}
+                <ProjectDesc className="my-4 sm:my-8"/>
+                <Separator className="my-4 sm:my-8" />
+                <Flexer className="my-4 sm:my-8" />
+                <TokenomicsSection className="my-4 sm:my-8" />
+                <StakeSection className="my-4 sm:my-8" />
+                <Separator className="my-4 sm:my-8" />
+                <Footer className="my-4 sm:my-8" />
             </div>
         </div>
     )
@@ -98,18 +119,25 @@ function NavBar(){
     ///handle menu open and close || switching of menu button icon
     // i'm only using icon value to conditionally display menu tray on small screens because of speed 
     const handleOpenMenu = () => setIcon(oldValue => oldValue === 'menu' ? 'x' : 'menu');
+    
 
     return(
-        <nav className="px-0 py-4 flex justify-between items-center sticky top-0 left-auto z-50  w-full bg-[#003338]">
-            <a href="#" className="text-xl text-white no-underline font-bold block">FIxTime</a>
-            <button type="button" onClick={handleOpenMenu}
-                className="capitalize font-bold text-sm p-4 text-white block md:hidden"
-            >
-                <i className={`bx bx-${icon} font-bold text-sm`}></i>
-            </button>
-            <div className={`w-fit flex flex-col p-4 md:p-0 absolute top-[100%] right-0 md:relative md:flex-row md:top-0 gap-3 items-center items-center bg-[#003338] ${icon === 'menu' ? 'invisible' : 'visible'} md:visible transition-all duration-300 `}>
-                {nav_links_data.map(link => (<NavLink link={link.link} title={link.title} key={link.title} />))}
-                <FilledBtn title='console' onClick={()=>alert('action unavailable')} />
+        <nav className="px-0 py-4 sticky top-0 left-auto z-50  w-full bg-[#003338]">
+            <div className="flex justify-between items-center w-full">
+                <a href="#" className="no-underline block">
+                    <img src="./assets/img/Logo.png" alt="our logo" 
+                        className="h-[1.5rem]"
+                    />
+                </a>
+                <button type="button" onClick={handleOpenMenu}
+                    className="capitalize font-bold text-sm p-0 text-white block md:hidden"
+                >
+                    <i className={`bx bx-${icon} bx-md`}></i>
+                </button>
+                <div className={`w-[105%]  md:w-fit h-[50vh] md:h-fit  flex flex-col p-4 md:p-0 absolute top-[100%] -right-[2.5%] md:relative md:flex-row md:top-0 md:right-0 gap-3 items-center items-center justify-center bg-[#003338] ${icon === 'menu' ? 'invisible' : 'visible'} md:visible transition-all duration-300 `}>
+                    {nav_links_data.map(link => (<NavLink link={link.link} title={link.title} key={link.title} />))}
+                    <FilledBtn title='console' onClick={()=>alert('action unavailable')} />
+                </div>
             </div>
         </nav>
     )
@@ -118,12 +146,25 @@ function NavBar(){
 ///HERO SECTION
 function HeroSection({className =''}){
     return(
-        <section className={`w-full p-4 relative flex flex-col items-center justify-center bg-[#161616] rounded-[1.5rem]  ${className}`}>
-             <div class="absolute inset-0 bg-[url('/assets/hero.png')] bg-no-repeat bg-cover bg-center opacity-60 rounded-[1.5rem]"></div>
-            <h1 className="text-white text-6xl text-center font-bold">FIxTime</h1>
-            <h6 className="text-white text-lg text-center">Make time more rewarding.</h6>
+        <section className={`w-full p-4 relative flex flex-col items-center justify-center bg-[#161616] rounded-[1.5rem] overflow-hidden  ${className}`}>
 
-            <a href="#projectDesc" className="block absolute bottom-4 z-10 animate-bounce text-[rgba(255,255,255,0.8)] no-underline">
+            <video 
+                autoPlay 
+                loop
+                muted 
+                playsInline
+                className="h-full w-full min-w-full min-h-full object-cover  absolute z-[1]"
+            >
+                <source src="./assets/vids/hero-vid.mp4" type="video/mp4" />
+                <div className="absolute inset-0 bg-[url('/assets/hero.png')] bg-no-repeat bg-cover bg-center opacity-60 rounded-[1.5rem]"></div>
+            </video>
+            <div className="absolute w-full h-full  z-[2] bg-[rgba(0,0,0)] opacity-40 rounded-[1.5rem]"></div>
+            <div className="relative z-[3]">
+                <h1 className="text-white text-6xl text-center font-bold">FIxTime</h1>
+                <h6 className="text-white text-lg text-center">Make time more rewarding.</h6>
+            </div>
+
+            <a href="#projectDesc" className="block absolute bottom-4 z-10 animate-bounce text-[rgb(255,255,255)] no-underline">
                 <span className="w-fit mx-auto block">
                     Learn more
                 </span>
@@ -134,12 +175,12 @@ function HeroSection({className =''}){
 }
 
 // Project Description
-function ProjectDesc() {
+function ProjectDesc({className = ''}) {
   return (
-    <section id="projectDesc" className="h-fit flex items-center flex-col sm:flex-row">
-        <div className="bg-[#191B1A] rounded-[1.5rem] p-6 h-fit flex-1 order-2 sm:order-1">
+    <section id="projectDesc" className={`h-fit flex items-stretch flex-col sm:flex-row gap-4 ${className}`}>
+        <div className="bg-[#191B1A] rounded-[1.5rem] p-6 flex-1">
             
-            <img src="./assets/circle-text.png" alt="circle-text" 
+            <img src="./assets/img/circle-text.png" alt="circle-text" 
                 className="w-[6rem] h-[6rem] object-center object-contain mb-4 ml-auto" 
             />
             <div className="flex flex-col items-start w-full gap-2 mb-2">
@@ -148,18 +189,20 @@ function ProjectDesc() {
                 <p className="text-[rgba(255,255,255,0.6)]">FlxTime is a project that allows miners (Flexers) to mine tokens using the method they want—via Crankk, ESP32-S3 board, Docker, and more.</p>
             </div>
         </div>
-        <div className="px-[6rem] p-4 order-1 sm:order-2">
-            <h1 className="text-3xl font-bold text-white">FIxTime</h1>
+        <div className=" w-full sm:w-[30%] flex-none flex items-center justify-center p-4 rounded-[6vh] rounded-bl-[0.33rem] logo-wrapper ">
+            <img src="./assets/img/colored-logo.png" alt="our logo" 
+                className="w-auto h-[10rem] sm:w-full sm:h-auto object-contain object-center"
+            />
         </div>
     </section>
   )
 }
 
 //  BECOME A FLEXER SECTION
-function Flexer() {
+function Flexer({className = ''}) {
     return(
-        <div className="flex flex-col gap-4 items-center py-4">
-            <h2 className=" text-2xl text-white w-fit">Ready to become a Flexer ?</h2>
+        <div className={`flex flex-col gap-6 items-center ${className}`}>
+            <h2 className=" text-2xl text-white w-fit text-center sm:text-left">Ready to become a Flexer ?</h2>
             <div className="flex gap-3 items-stretch justify-center flex-col sm:flex-row">
                 {flex_cards.map(({description, id, icon, title})=>(<FlexerCard icon={icon} title={title} desc={description} key={id} />))}
             </div>
@@ -169,13 +212,13 @@ function Flexer() {
 }
 
 // Tokenomics Section Component
-function TokenomicsSection() {
+function TokenomicsSection({className = ''}) {
     return (
-      <section className="bg-[#011208] text-white p-6 sm:p-8  md:p-16  rounded-[1.5rem]">
-        <div className="max-w-7xl mx-auto px-6">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-            <TokenomicsDescription />
-            <TokenomicsDiagram />
+      <section className={`bg-[#011208] text-white py-8 px-6 sm:p-8  lg:p-16  rounded-[1.5rem] ${className}`}>
+        <div className="max-w-7xl mx-auto ">
+          <div className="flex flex-col lg:flex-row gap-8 items-center overflow-hidden">
+            <TokenomicsDescription className="flex-1 lg:flex-[40%]"/>
+            <TokenomicsMap className="flex-1 lg:flex-[60%]" />
           </div>
         </div>
       </section>
@@ -183,10 +226,10 @@ function TokenomicsSection() {
 }
 
 // Stake Section Component
-function StakeSection() {
+function StakeSection({className =''}) {
     return (
-        <div className="bg-[#191B1A] py-16 px-4 md:pl-16 md:px-8 h-fit relative rounded-[1.5rem] bg-[url('/assets/stack.png')] bg-[30%] bg-[length:auto_40%]  md:bg-contain bg-right bg-no-repeat">
-            <div class="absolute inset-0 bg-[#191B1A]  mix-blend-screen rounded-[1.5rem]"></div>
+        <div className={`bg-[#191B1A] py-16 px-4 md:pl-16 md:px-8 h-fit relative rounded-[1.5rem] bg-[url('/assets/img/stack.png')] bg-[30%] bg-[length:auto_40%]  md:bg-contain bg-right bg-no-repeat ${className}`}>
+            <div className="absolute inset-0 bg-[#191B1A]  mix-blend-screen rounded-[1.5rem]"></div>
             <h2 className="text-2xl font-bold mb-4 text-white">Stake your time</h2>
             <p className=" text-lg text-[rgba(255,255,255,0.8)]">
               Coming soon! Stake your $SAMPLE and earn more rewards.
@@ -196,10 +239,10 @@ function StakeSection() {
 }
   
 // Footer Component
-function Footer() {
+function Footer({className=''}) {
     return (
-        <footer className="py-8">
-            <div className="max-w-7xl mx-auto px-6 text-left">
+        <footer className={`pt-8 pb-16 ${className=''}`}>
+            <div className="max-w-7xl mx-auto text-left">
               <p className="text-sm text-white">FlxTime • 2024</p>
               <div className="flex flex-col justify-center items-start  mt-4 text-gray-400">
                 {footer_links.map(link=>(<FooterLinks {...link} key={link.title} />))}
@@ -231,65 +274,52 @@ function NavLink({title, link}){
 
   
 // Tokenomics Description Component
-function TokenomicsDescription() {
+function TokenomicsDescription({className=''}) {
     return (
-        <div className="text-[rgba(255,255,255,0.8)] text-center sm:text-left">
+        <div className={`text-[rgba(255,255,255,0.8)] text-center lg:text-left ${className}`}>
             <h2 className="text-3xl font-bold mb-0 text-white">Tokenomics</h2>
             <p className="text-sm mb-4">/ flex-o-nomics /</p>
             <p className="mb-6 text-lg">
-              $SAMPLE is the primary token of FlxTime for rewards and governance.
+              $FLXT is the primary token of FlxTime for rewards and governance.
             </p>
-            <FilledBtn title='read more in docs' icon="right-arrow-alt" className='mx-auto mx-0' />
+            <FilledBtn title='read more in docs' icon="right-arrow-alt" className='mx-auto lg:mx-0' />
         </div>
     );
 }
   
-// Tokenomics Diagram Component (Image Slot)
-function TokenomicsDiagram() {
-    return (
-        <div className="relative flex justify-center items-center">
-          <img
-            src="/assets/tokenomics.png"
-            alt="Tokenomics Diagram"
-            className="w-full h-auto sm:w-64 sm:h-64 object-contain"
-          />
-        </div>
-    );
-}
-
 
   
 
 ///FILLED BUTTON
 // icon is the name of the icon to be placed in the button  NOTE: all valid names are available at https://boxicons.com/
-function FilledBtn({title, icon='', className ='', onClick}){
+function FilledBtn({title, icon='', className ='', href}){
     return(
-        <button type="button" onClick={onClick}
-            className={`capitalize font-bold text-[#190D13] bg-[#29EEFF] text-sm flex gap-1 items-center justify-center py-2 px-4 rounded-[0.5rem] ${className}`}
+        <a href={href}
+            className={`capitalize font-bold text-[#190D13] bg-[#29EEFF] text-sm flex gap-1 items-center justify-center py-2 px-4 rounded-[0.5rem] w-fit ${className}`}
         >
             <span>
                 {title}
             </span>
             {icon && <i className={`bx bx-${icon} font-bold text-sm`}></i>}
-        </button>
+        </a>
     )
 }
 
 //FLEXER CARD
 function FlexerCard({title, desc, icon}) {
     return(
-        <div className="bg-[#191B1A] w-full sm:w-[33%] md:w-[20%]  rounded-[1.5rem] px-4 py-6 flex flex-col gap-2 items-center justify-center">
-            <img src={icon} alt={`${title} icon`} className="w-[1.2rem] h-[1.2rem]" />
-            <h6 className="text-base capitalize text-white text-center font-bold">{title}</h6>
-            <p className="text-xs text-[rgba(255,255,255,0.6)] text-center">{desc}</p>
+        <div className="bg-[#191B1A] w-full sm:w-[33%] md:w-[20%]  rounded-[1.5rem] px-4 py-8 flex flex-col gap-2 items-center justify-start">
+            <img src={icon} alt={`${title} icon`} className="w-[2.5rem] h-[2.5rem] m-3 " />
+            <h6 className="text-2xl capitalize text-white text-center font-bold">{title}</h6>
+            <p className="text-sm text-[rgba(255,255,255,0.6)] text-center">{desc}</p>
         </div>
     )
 }
 
 ///SEPARATOR
-function Separator() {
+function Separator({className =''}) {
     return(
-        <div className="w-full h-[8rem] bg-[url('/assets/Separator.png')] bg-cover bg-no-repeat bg-center"></div>
+        <div className={`w-full h-[3rem] sm:h-[8rem] bg-[url('/assets/img/Separator.png')] bg-cover bg-no-repeat bg-center ${className}`}></div>
     )
 }
 
@@ -298,10 +328,39 @@ function FooterLinks({title, icon, link}) {
     return(
         <a href={link} className="text-gray-400 hover:text-white group flex items-center gap-2 max-w-full">
             {icon && <img src={icon} className="w-[1.2rem] h-[1.2rem] object-contain  object-center"/>}
-            <span className="text-ellipsis overflow-hidden">{title}</span>
+            <span className="text-ellipsis capitalize overflow-hidden text-nowrap">{title}</span>
         </a>
     )
 }
+
+/// TOKENOMICS DIAGRAM
+
+/// MAIN MAP
+function TokenomicsMap({className = ''}) {
+    return(
+        <div className={`flex items-center justify-center flex-col min-[580px]:flex-row -mb-[5rem] min-[580px]:mb-0 overflow-hidden gap-3 ${className}`}>
+            <img src="./assets/img/coin-img.png" alt="our token" className=" w-[90%] h-auto min-[580px]:w-[14rem] min-[580px]:h-[14rem] object-contain object-center order-2 min-[580px]:order-1" />
+            <div className="flex flex-col gap-1 order-1 min-[580px]:order-2" >
+                {
+                    token_map.map((map, index)=>(<MapPoint {...map} key={map.title + ' ' + index} className={`ml-0 ${index > 0 && index < 3 ? 'min-[580px]:ml-[1rem]' : 'min-[580px]:-ml-[1.2rem]' }`} />))
+                }
+            </div>
+        </div>
+    )
+}
+
+/// MAP POINTS
+function MapPoint({icon, title, className=''}) {
+    return(
+        <div className={`text-white capitalize flex items-center gap-4 w-fit p-2 transition duration-300 ${className}`}>
+            <img src={icon} alt={`${title} icon`} className="w-[4rem] h-[4rem] object-contain object-center" />
+            <h6 className="text-base font-bold">
+                {title}
+            </h6>
+        </div>
+    )
+}
+
 
 ///MOUNTS APP
 ReactDOM.createRoot(document.getElementById('app')).render(<App />)
